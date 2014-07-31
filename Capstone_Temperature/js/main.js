51b5b8ec87012b9b69033b5262ed3e6e5f539264
Jairo <JJEcu13@hotmail.com>
2014-07-31 12:56:27 -0400

    changed temperature degrees

--- Capstone_Temperature/js/main.js
+++ Capstone_Temperature/js/main.js
@@ -9,13 +9,13 @@ $(document).ready(function() {
 		// Sets and updates value in realtime
 		$("#datastream-value").xively('live', {feed: 1502428704, datastream: 'capstone_temperature'});
 
-		if(temperature <= 74) { // if temperature is 74 degrees or lower, the cool.css will be used making the background white and changing the gif to a fan with gray blades
+		if(temperature <= 76) { // if temperature is 74 degrees or lower, the cool.css will be used making the background white and changing the gif to a fan with gray blades
 			$("#css").attr("href", "css/cool.css");
 			$("#temp_image").attr("src", "images/cool.gif");
-		} else if(temperature <= 79 && temperature >= 75) { // if temperature is between 79 and 75 degrees, the hot_cool.css will be used making the background green and changing the gif to a fan with orange blades
+		} else if(temperature == 77) { // if temperature is between 79 and 75 degrees, the hot_cool.css will be used making the background green and changing the gif to a fan with orange blades
 			$("#css").attr("href", "css/hot_cool.css");
 			$("#temp_image").attr("src", "images/hot_cool.gif");
-		} else if(temperature >= 80) { // if temperature is 80 degrees or higher, the sunny.css will be used making the background orange and changing the gif to a sun
+		} else if(temperature >= 78) { // if temperature is 80 degrees or higher, the sunny.css will be used making the background orange and changing the gif to a sun
 			$("#css").attr("href", "css/sunny.css");
 			$("#temp_image").attr("src", "images/sunny.gif");
 		}
